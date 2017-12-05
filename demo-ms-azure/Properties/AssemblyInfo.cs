28a7a810ee3a980be102560b366852ed11dcfb5b
Paddyk3ll3h3r <32489324+Paddyk3ll3h3r@users.noreply.github.com>
2017-12-05 11:54:01 +0000

    Update AssemblyInfo.cs

--- demo-ms-azure/Properties/AssemblyInfo.cs
+++ demo-ms-azure/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // l’ensemble d’attributs suivant. Changez les valeurs de ces attributs pour modifier les informations
 // associées à un assembly.
 [assembly: AssemblyTitle("demo_ms_azure")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("test")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("demo_ms_azure")]
